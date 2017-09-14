8fd5aaac451b64585a2f49d9877a153d71c1ce84
Martin <martin_deluca@hotmail.it>
2017-09-14 17:23:13 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
 
 int valueInList(char*,char*);
+int valueNotInList(char*,char*);
+int valueInListInt(long long int , char*);
+int valueNotInListInt(long long int , char*);
 
-
-main()
+int main()
 {
     char *string = NULL;
+    string = "9930|9998|9999";
+    int x = 10000;
 
-    string = "Ori|Boldi|C|D|E|F|G|H|I|L|M|N|O";
+    /* **********************
+     * Examples
+     *
+     * valueNotInListStr("10000",string);
+     * valueInListStr("10000",string);
+     *
+     *
+     * valueNotInListInt(x,string);
+     * valueInListInt(10000,string);
+     */
 
-    return valueInList("Z",string);
+    return valueNotInListStr("10000",string);
 }
 
-int valueInList(char *value, char *ValueList)
+/* ****************************************************
+ * Author:    Martin De Luca
+ * Date:      14/09/2017
+ * Version:   2.0
+ * ***************************************************/
+int valueInListStr(char *value, char *ValueList)
 {
     if(value == NULL || ValueList == NULL)
         return -1;
 
-    char ** res  = NULL;
     const char *sep = "|";
     char *tok = NULL;
-    int n_sep = 0,i;
 
     char *ValueListCopy = (char*) malloc(strlen(ValueList)*sizeof(char)+1);
     strcpy(ValueListCopy,ValueList);
@@ -33,33 +50,121 @@ int valueInList(char *value, char *ValueList)
         return -1;
     }
 
-    /* split string and append tokens to 'res' */
     while(tok)
-    {   res = (char**) realloc (res, sizeof (char*) * ++n_sep);
-        if(res == NULL)
-        {   free(res);
-            free(ValueListCopy);
-            return -1; /* memory allocation failed */
+    {   if(strcmp(value,tok)==0)
+        {   free(ValueListCopy);
+            return 1;
         }
-
-        res[n_sep-1] = tok;
         tok = strtok (NULL, sep);
     }
 
-    /* realloc one extra element for the last NULL */
-    res = (char**) realloc (res, sizeof (char*) * (n_sep+1));
-    res[n_sep] = 0;
-
-    for (i=0; i < n_sep;++i)
-    {   if(strcmp(value,res[i])==0)
-        {   return 1;
-            free(res);
-            free(ValueListCopy);
-        }
-    }
-
-    free(res);
     free(ValueListCopy);
     return 0;
+}
+
+/* ****************************************************
+ * Author:    Martin De Luca
+ * Date:      14/09/2017
+ * Version:   2.0
+ * ***************************************************/
+int valueNotInListStr(char *value, char *ValueList)
+{
+    if(value == NULL || ValueList == NULL)
+        return -1;
+
+    const char *sep = "|";
+    char *tok = NULL;
+
+    char *ValueListCopy = (char*) malloc(strlen(ValueList)*sizeof(char)+1);
+    strcpy(ValueListCopy,ValueList);
+
+    tok = strtok(ValueListCopy,sep);
+    if(tok==NULL)
+    {   free(ValueListCopy);
+        return -1;
+    }
+
+    while(tok)
+    {   if(strcmp(value,tok)==0)
+        {   free(ValueListCopy);
+            return 0;
+        }
+        tok = strtok (NULL, sep);
+    }
+
+    free(ValueListCopy);
+    return 1;
+}
+
+/* ****************************************************
+ * Author:    Martin De Luca
+ * Date:      14/09/2017
+ * Version:   2.0
+ * ***************************************************/
+int valueInListInt(long long int value, char *ValueList)
+{
+    if(ValueList == NULL)
+        return -1;
+
+    const char *sep = "|";
+    char *tok = NULL;
+    long long int tok_int=0;
+
+    char *ValueListCopy = (char*) malloc(strlen(ValueList)*sizeof(char)+1);
+    strcpy(ValueListCopy,ValueList);
+
+    tok = strtok(ValueListCopy,sep);
+    if(tok==NULL)
+    {   free(ValueListCopy);
+        return -1;
+    }
+
+    while(tok)
+    {   tok_int=strtoll(tok, NULL, 10);
+        if(value==tok_int)
+        {   free(ValueListCopy);
+            return 1;
+        }
+        tok = strtok (NULL, sep);
+    }
+
+    free(ValueListCopy);
+    return 0;
+}
+
+/* ****************************************************
+ * Author:    Martin De Luca
+ * Date:      14/09/2017
+ * Version:   2.0
+ * ***************************************************/
+int valueNotInListInt(long long int value, char *ValueList)
+{
+    if(ValueList == NULL)
+        return -1;
+
+    const char *sep = "|";
+    char *tok = NULL;
+    long long int tok_int=0;
+
+    char *ValueListCopy = (char*) malloc(strlen(ValueList)*sizeof(char)+1);
+    strcpy(ValueListCopy,ValueList);
+
+    tok = strtok(ValueListCopy,sep);
+    if(tok==NULL)
+    {   free(ValueListCopy);
+        return -1;
+    }
+
+    while(tok)
+    {   tok_int=strtoll(tok, NULL, 10);
+        if(value==tok_int)
+        {   free(ValueListCopy);
+            return 0;
+        }
+        tok = strtok (NULL, sep);
+    }
+
+    free(ValueListCopy);
+    return 1;
 }
 
